ed58346a03e989efc4defd164bcd899ec66e9346
KanonM2014 <shaka.pempek@gmail.com>
2026-04-20 17:37:49 +0700

    init

---
+++ test.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def halaman_utama():
+    nama_warung = "Warung Serba Ada"
+    return render_template('index.html', warung=nama_warung)
+
+@app.route('/produk')
+def halaman_produk():
+    return render_template('produk.html')
+
+# Pastikan 'if' menggunakan huruf kecil
+if __name__ == '__main__':
+    app.run(debug=True)
